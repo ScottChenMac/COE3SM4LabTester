7462b3ca75d9ec7edf9a7b1d7a6e0b90bebe13b2
Scott Chen <schen@conestogac.on.ca>
2024-11-17 21:49:11 -0500

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        int a = 3, b = 4;
+        int result = a + 2 * b;
+        System.out.printf("%d + %d = %d\n", a, b, result);
+    }
+}
