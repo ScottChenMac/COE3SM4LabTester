53e698e10c3cdc9971b5206bcbfa9fd03322a264
Scott Chen <schen@conestogac.on.ca>
2024-11-19 14:59:47 -0500

    updated

--- TestHelloWorld.java
+++ TestHelloWorld.java
@@ -1,10 +1,36 @@
-import static org.junit.Assert.assertEquals;
-import org.junit.Test;
+import java.io.*;
 
 public class TestHelloWorld {
-    @Test
-    public void Test1()
+    
+    private static boolean passed = true;
+    
+    public static void Test1()
     {
-        assertEquals(2, 1+1);
-    }    
+        PrintStream originalOut = System.out;
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(bos));
+
+        // action
+        HelloWorld.main(null);
+
+        // assertion
+        passed &= assertEqualString("3 + 4 = 11 \n", bos.toString());
+
+        // undo the binding in System
+        System.setOut(originalOut);
+    }   
+    
+    public static void main(String[] args)
+    {
+        Test1();
+
+        // so to force a usable return code
+        if(passed)  System.exit(0);
+        else        System.exit(-1);
+    }
+
+    private static boolean assertEqualString(String a, String b)
+    {
+        return a.equals(b);
+    }
 }
